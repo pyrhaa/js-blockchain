b70f0f0312906604a77abc868cd8cbda199f44c7
pyrhaa <sboubtane@protonmail.com>
2021-09-29 15:08:30 +0200

    temporary installations

---
+++ controllers/controllers.js
@@ -0,0 +1,18 @@
+class MyBank {
+  login = (req, res, next) => {
+    next();
+  };
+
+  getAccount = (req, res) => {
+    next();
+  };
+
+  updateAccount = (req, res) => {
+    next();
+  };
+
+  approveAccount = (req, res) => {
+    next();
+  };
+}
+export default new MyBank();
