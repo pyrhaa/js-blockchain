72fddf805bc9be586611546651dbdadc6b767b33
pyrhaa <sboubtane@protonmail.com>
2021-09-29 17:26:13 +0200

    chain ok

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const SHA256 = require('crypto-js/sha256');
 
+//9:18
 //the template of every block in our chain that will contain infos of each current block and his previous block
 //calculateHash() ::: calculate and return hash of this block with the constructor property
 class Block {
@@ -18,8 +19,29 @@ class Block {
   }
 }
 
+//this.chain ::: an array that contain genesis block at index 0
 class Blockchain {
   constructor() {
-    this.chain = [];
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, '29/09/2021', 'Genesis Block', '0');
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.prevHash = this.getLatestBlock().currentHash;
+    newBlock.currentHash = newBlock.calculateHash();
+    this.chain.push(newBlock);
   }
 }
+
+let azulAmazCoin = new Blockchain();
+azulAmazCoin.addBlock(new Block(1, '30/09/2021', { amount: 4 }));
+azulAmazCoin.addBlock(new Block(2, '31/09/2021', { amount: 11 }));
+
+console.log(JSON.stringify(azulAmazCoin, null, 4));
