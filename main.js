3f2a2d5030ed4afecb4b94d2e02e0dfefe6ba6be
pyrhaa <sboubtane@protonmail.com>
2021-09-29 16:06:54 +0200

    set calculateHash() with sha256

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const SHA256 = require('crypto-js/sha256');
+
 //the template of every block in our chain that will contain infos of each current block and his previous block
 //calculateHash() ::: calculate and return hash of this block with the constructor property
 class Block {
@@ -9,5 +11,9 @@ class Block {
     this.currentHash = '';
   }
 
-  calculateHash() {}
+  calculateHash() {
+    return SHA256(
+      this.index + this.prevHash + this.timestamp + JSON.stringify(this.data)
+    ).toString();
+  }
 }
