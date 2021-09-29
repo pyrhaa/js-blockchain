297092a28726fe8b85863abec688216e0657feda
pyrhaa <sboubtane@protonmail.com>
2021-09-29 15:49:41 +0200

    restart freshly, set Block

---
+++ main.js
@@ -0,0 +1,9 @@
+class Block {
+  constructor(index, timestamp, data, prevHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.prevHash = prevHash;
+    this.currentHash = '';
+  }
+}
