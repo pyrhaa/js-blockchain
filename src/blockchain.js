6d97cc1df42e3949893840833e4f44bf86a4ccb0
pyrhaa <sboubtane@protonmail.com>
2021-10-06 14:39:39 +0200

    publicKey verify equal to signature and calculateHash

--- src/blockchain.js
+++ src/blockchain.js
@@ -27,6 +27,9 @@ class Transaction {
     if (!this.signature || this.signature.length === 0) {
       throw new Error('No signature in this transaction');
     }
+
+    const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+    return publicKey.verify(this.calculateHash(), this.signature);
   }
 }
 
